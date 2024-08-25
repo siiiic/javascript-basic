0057a93bacc03ed833b358aa3b4bc8a252351306
Danil <siiiic@yandex.ru>
2024-08-25 20:35:47 +0300

    Приветствия

---
+++ 5-languages/languages.js
@@ -0,0 +1,20 @@
+const language = `jp`; 
+switch(language) {
+    case 'en': 
+        console.log('Hello');
+    break;
+    case 'de':
+        console.log('Gutten tag');
+    break;
+    case 'ru':
+        console.log('Привет');
+    break;
+    case 'jp':
+        console.log('こんにちは');
+    break;
+    case 'sw':
+        console.log('Hej');
+    break;
+    default:
+        console.log('Hello');
+}
