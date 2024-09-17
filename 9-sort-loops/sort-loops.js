eacee316077ae23ed8a511351c782aa0a5626193
Danil <siiiic@yandex.ru>
2024-09-17 13:54:02 +0300

    Сократил кол-во иттераций, добавил генерацию массива со случайными числами и напрпавление сравнения, исправил мутацию исходного массива

--- 9-sort-loops/sort-loops.js
+++ 9-sort-loops/sort-loops.js
@@ -1,13 +1,25 @@
-const arr = [-4, 24, 43, 1, -40, -5, 88, -7, 10, 0];
+const numbers = Array.from({length: 100}, () => {
+    const factor = Math.random() > 0.5 ? 1 : -1;
+    const randomNumber = Math.ceil(Math.random() * 1000 * factor);
+    return randomNumber;
+});
 
-function sort (array) {
+function template (firstNumber, secondNumber, condition) {
+    return !condition ? firstNumber > secondNumber : secondNumber > firstNumber;
+}
+
+function sort (incomingArray, condition = false) {
+    const array = [...incomingArray]
     for(let i = 0; i < array.length; i++)
-        for(let j = 0; j < array.length; j++) {
-        if (array[i] < array[j]) {
+        for(let j = i; j < array.length; j++) {
+        const isExchange = template(array[i], array[j], condition)
+        if (isExchange) {
             [array[j], array[i]] = [array[i], array[j]];
         }
     }
     return array;
 }
 
-console.log(sort(arr));
+console.log(numbers);
+console.log(`Массив отсортирован по возрастанию: ${sort(numbers)} \n`);
+console.log(`Массив отсортирован по возрастанию: ${sort(numbers)}`);
