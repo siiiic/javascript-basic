a495cd2d5e1f09d8b0cc32d1b4ca304bbec989af
Danil <siiiic@yandex.ru>
2024-09-17 13:42:44 +0300

    Добавил генерацию случайного пароля, изменил способ шифровки

--- 8-crypto/crypto.js
+++ 8-crypto/crypto.js
@@ -1,19 +1,45 @@
-function crypto (password) {
+function getRandomNumber (num = 90) {
+    return Math.ceil(Math.random() * num)
+}
+
+function getRandomPassword(length = 12) {
+    const symbolArray = Array.from({ length: 90 }, (_, index) => String.fromCharCode(index + 33));
+    const password = Array.from({ length }, () => symbolArray[getRandomNumber()]);
+    return password.join('');
+}
+
+function crypto (password) { 
+    if (!password || !password.length) {
+        return NaN;
+    }
     const array = password.split('');
-    const sliced = array.splice(password.length - 3);
-    const encrypted = sliced.concat(array)
-        .reverse()
-        .join('');
-    return encrypted;
+    if(array.length < 5) {
+        return null;
+    } 
+    const middleIndex = Math.floor(array.length / 2);
+    const firstHalf = array.slice(0, middleIndex).reverse();
+    const secondHalf = array.slice(middleIndex).reverse();
+    const length = secondHalf.length;
+    [secondHalf[1], secondHalf[length - 2] = secondHalf[length - 2], secondHalf[1]];
+    const encrypted = firstHalf.concat(secondHalf);
+    return encrypted.join('')
 }
 
-function decrypted (encrypted, password) {
-    return crypto(password) === encrypted ? true : false;
+function checkPassword (encrypted, original) {
+    return original === crypto(encrypted);
 }
 
-console.log(crypto('purple'));
-console.log(decrypted('rupelp', 'purple'));
-console.log(crypto('qWerTy1234'));
-console.log(decrypted('1yTreWq432', 'qWerTy1234'));
-console.log(crypto('Не сработал((('));
-console.log(decrypted('латобарс еН(((', 'Не сработал)))'));
+
+const password = getRandomPassword(8);
+const encrypted = crypto(password);
+
+console.log(`Password: ${password}`);
+console.log(`Encrypted password: ${encrypted}`);
+console.log(`Password verification: ${checkPassword(encrypted, password)}`);
+
+const password2 = getRandomPassword();
+const encrypted2 = crypto(password2).substring(1);
+
+console.log(`Password: ${password2}`);
+console.log(`Encrypted password: ${encrypted2}`);
+console.log(`Password verification: ${checkPassword(encrypted2, password2)}`);
