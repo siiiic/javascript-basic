8f587218b02389107c0e76d3d334ecd2a03d6525
Danil <siiiic@yandex.ru>
2024-09-17 13:22:09 +0300

    Переделал с использованием switch

--- 7-convert/convert.js
+++ 7-convert/convert.js
@@ -1,13 +1,39 @@
-function convert (amount, currency, targetСurrency) {
-    const exchangeRate = {
-        rubusd: 0.01,
-        usdrub: 95,
-        eurusd: 1.11,
-        usdeur: 0.9,
-        rubtry: 0.4,
-        tryrub: 2.7
-    }
-    return amount * exchangeRate[currency.toLowerCase() + targetСurrency.toLowerCase()] || null;
+function convert (sum, currency, targetСurrency) {
+    const USD = 90;
+    const EUR = 100;
+    currency = currency.toUpperCase();
+    targetСurrency = targetСurrency.toUpperCase();
+    switch (currency) {
+        case ('USD'): 
+            switch (targetСurrency){
+                case ('RUB'):
+                    return (sum * USD).toFixed(2);
+                case ('EUR'):
+                    return ((sum * USD) / EUR).toFixed(2);
+                default:
+                    return null;
+            }
+        case ('EUR'):
+            switch (targetСurrency) {
+                case ('RUB'):
+                    return (sum * EUR).toFixed(2);
+                case ('USD'):
+                    return ((sum * EUR) / USD).toFixed(2);
+                default:
+                    return null;
+            }
+        case ('RUB'):
+            switch (targetСurrency) {
+                case ('USD'):
+                    return (sum / USD).toFixed(2);
+                case ('EUR'):
+                    return (sum / EUR).toFixed(2);
+                default:
+                    return null;
+            }
+        default:
+            return null
+        }
 }
 
-console.log(convert(1000, `eUr`, `usD`));
+console.log(convert(1000, `eur`, `usd`));
