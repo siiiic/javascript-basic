a5891d104dbb51a197bbb49c6d640132985ef7af
Danil <siiiic@yandex.ru>
2024-10-09 11:03:39 +0300

    Правки
    1. Изменил функицю getTask
    2. ID присваивается по внешнему каунтеру
    3. Добавил проверки
    4. Объеденил функции изменения названия задачи и приоритета в одну
    5. Сортировка по приоритету теперь работает в обе стороны
    6. Добавил вывод сообщений

--- 15-task-object/task-object.js
+++ 15-task-object/task-object.js
@@ -1,39 +1,112 @@
 'use strict';
 
+const _ = undefined;  // Использовать для изменения приоритета задачи без изменения заголовка updateTitlePriority(id, _, priority)
+
 const toDoList = {
     list: [],
-    getTask: function (id) {
-        return this.list.findIndex(task => task.id === id);
+    idCounter: 0,
+    getTask: function (taskId) {
+        return this.list.find(({ id }) => id === taskId);
     },
-    addTask: function (title, priority) {
-        const id = this.list.length + 1;
-        this.list.push({title, id, priority});
+    addTask: function (task) {
+        let message = 'Task entry error';
+
+        if (!task) {
+            return message;
+        }
+
+        task.id = this.idCounter++; 
+        this.list.push(task);
+        return message = `The task with id: ${task.id} successfully added`;
     },
     deleteTask: function (id) {
-        this.getTask(id) === -1 ?
-        console.log(`Задачи с id: ${id} нет в списке`) :
-        this.list.splice(this.getTask(id), 1);
+        let message = `The task with id: ${id} was not found`;
+        const isAdded = this.getTask(id);
+
+        if (!isAdded) {
+            return message;
+        }
+
+        this.list = this.list.filter(task => task.id !== isAdded.id);
+        return message = `The task with id: ${id} successfully deleted`;
     },
-    sortByPriority: function () {
-        this.list.sort((a, b) => a.priority - b.priority);  
+    sortByPriority: function (statment = true) {
+        let message = `There should be more than one task`;
+        if(this.list.length <= 1) {
+            return message;
+        }
+
+        this.list.sort((a, b) => 
+        statment ?
+        a.priority - b.priority : 
+        b.priority - a.priority);
+
+        statment ? 
+        message = `Tasks sorted in ascending order` :
+        message = `Tasks sorted in descending order`;
+        return message;
     },
-    updateTitle: function (id, title) {
-        this.list[this.getTask(id)].title = title;
-    },
-    updatePriority: function (id, priority) {
-        this.list[this.getTask(id)].priority = priority;
+    updateTitlePriority: function (id, title = undefined , priority = undefined) {
+        let message = `ID entry error`
+        const task = this.getTask(id);
+
+        if (!task) {
+            return message;
+        }
+
+        switch(true) {
+            case (title !== undefined && priority !== undefined):
+                task.title = title;
+                task.priority = priority;
+                return message = `In task with id: ${id} title and priority successfully changed`;
+            case (title !== undefined):
+                task.title = title;
+                return message = `In task with id: ${id} title successfully changed`;
+            case (priority !== undefined): 
+                task.priority = priority;
+                return message = `In task with id: ${id} priority successfully changed`;
+            default: 
+            return message = 'Enter title or priority';
+        }
     }
-}
+};
 
-toDoList.addTask('Помыть посуду', 2);
-toDoList.addTask('Заказать покушить', 3);
-toDoList.addTask('Встретиться с дружочками', 5);
-toDoList.addTask('Бильярд', 4);
-toDoList.addTask('Сделать дз', 1);
-toDoList.updateTitle(5, 'Не делать дз');
-toDoList.updatePriority(5, 6)
-toDoList.sortByPriority();
-toDoList.deleteTask(3);
-toDoList.deleteTask(100);
-console.log(toDoList);
+const Task1 = {
+    title: 'Покормить кота',
+    priority: 2
+};
 
+const Task2 = {
+    title: 'Купить пластинки',
+    priority: 1
+};
+
+const Task3 = {
+    title: 'Заказать пиццу',
+    priority: 3
+};
+
+const Task4 = {
+    title: 'Бильярд',
+    priority: 4
+};
+
+console.log(toDoList.addTask(Task1));
+console.log(toDoList.addTask(Task2));
+console.log(toDoList.addTask(Task3));
+console.log(toDoList.addTask(Task4));
+
+console.log(toDoList.list);
+
+console.log(toDoList.sortByPriority());
+console.log(toDoList.list);
+console.log(toDoList.sortByPriority(false));
+console.log(toDoList.list);
+
+console.log(toDoList.deleteTask(4));
+console.log(toDoList.deleteTask(3));
+console.log(toDoList.list);
+
+console.log(toDoList.updateTitlePriority(2, 'Приготовить пиццу'));
+
+console.log(toDoList.list);
